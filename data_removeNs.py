9628b2898f413ff6436bb231cf026d834630b948
ehilgart <ehilgart2015@gmail.com>
2021-11-10 11:17:34 -0500

    Final version of removing Ns.

--- data_removeNs.py
+++ data_removeNs.py
@@ -5,15 +5,15 @@ sys.path.insert(1, '/Users/eleanorhilgart/github/cg-66-contaminant-detection/dat
 
 from data_utils import read_fasta_files
 from fasta import FASTA
-from fasta import _read_file
 
 def write_solution(ID, seq, out_fh, per_line=60): #took the FASTA writing function from HW4
   offset = 0
-  out_fh.write(f">{ID}\n")
-  while offset < len(seq):
-    line = seq[offset:offset + per_line]
-    offset += per_line
-    out_fh.write(line + "\n")
+  with open(out_fh, 'w') as f:
+      f.write(f">{ID}\n")
+      while offset < len(seq):
+        line = seq[offset:offset + per_line]
+        offset += per_line
+        f.write(line + "\n")
 
 '''
 This will only need to be used once. The human genome files have a lot of Ns in them, which result from
@@ -27,17 +27,19 @@ outlst = [] #list of output files to be written to
 directory = '/Users/eleanorhilgart/Desktop/CHM1.1_V1_chromosomes_hum_iterate'
 for filename in os.listdir(directory):
     if filename.startswith("chr"):
-        ls.append(os.path.join(directory, filename))
+        ls.append(str(os.path.join(directory, filename)))
     elif filename.startswith("noN"):
         outlst.append(os.path.join(directory, filename))
     else:
         continue
 ls = sorted(ls)
 outlst = sorted(outlst)
+#print(ls)
+#print(outlst)
 
 for i in range(len(ls)):
-  fastafile = read_fasta_files(ls[i]) #read each file
-  tup = _read_file(fastafile) #parse the file
+  #fastafile = read_fasta_files(ls[i]) #read each file
+  lst = list(FASTA._read_file(ls[i])) #parse the file
   for char in 'Nn':
-      tup[0] = tup[0].replace(char, '')
-  write_solution(tup[1], tup[0], outlst[i]) #write to the output file
+      lst[0] = lst[0].replace(char, '')
+  write_solution(lst[1], lst[0], outlst[i]) #write to the output file
