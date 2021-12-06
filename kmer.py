0b0c97bdbd45a2f8bb1d9deafc70735c4471465e
gshatkin <gshatki1@jhmi.edu>
2021-12-06 17:36:55 -0500

    fixed bug

--- kmer.py
+++ kmer.py
@@ -7,18 +7,19 @@ def build_kmer_index(FA, k):
     #keys will be kmers and entries will be a list of where in the fasta the kmer starts
     idx_list = [] #list to store all the indices created from a multi fastsa file
     FA_list = [] #list to seperately store the FA's from a multi fasta file
-    for frag in FA: # TODO: I (Matthew) added this to support multi-fasta files (data is a list of strings instead of 1 string)
-        idx = {}
-        for i in range(len(frag)-k+1):
-            mer = frag[i:i+k]
-            #make new entries for new kmers
-            if mer not in idx:
-                idx[mer] = [i]
-            #append the location of repeated kmers to the list
-            elif mer in idx:
-                idx[mer].append(i)
-        idx_list.append(idx)
-        FA_list.append(frag)
+    for entry in FA:
+        for frag in entry: # TODO: I (Matthew) added this to support multi-fasta files (data is a list of strings instead of 1 string)
+            idx = {}
+            for i in range(len(frag)-k+1):
+                mer = frag[i:i+k]
+                #make new entries for new kmers
+                if mer not in idx:
+                    idx[mer] = [i]
+                #append the location of repeated kmers to the list
+                elif mer in idx:
+                    idx[mer].append(i)
+            idx_list.append(idx)
+            FA_list.append(frag)
 
     #return the completed kmer index
     return idx_list, FA_list
@@ -30,11 +31,13 @@ def kmer_engine(FQ, des_ref, cont_refs, k = 10, tolerance = 5): #requires the de
     #make indexes from desired references
     otpt = build_kmer_index(des_ref, k)
     des_idxs = otpt[0]
+    #print(des_idxs)
     des_FAs = otpt[1]
 
     #make indexes from contamination references
     otpt2 = build_kmer_index(cont_refs, k)
     cont_idxs = otpt2[0]
+    #print(cont_idxs)
     cont_FA = otpt2[1]
 
     #set the counts of desired and contaminant reads
@@ -53,11 +56,12 @@ def kmer_engine(FQ, des_ref, cont_refs, k = 10, tolerance = 5): #requires the de
     cont_reads = []
     count_good = []
     count_cont = []
-    
+
     for entry in FQ:
         read = entry['seq']
+        #print(read)
         which_read += 1
-        
+
         #keep track of where each read has been assigned
         in_des = 0
         in_cont = 0
@@ -68,35 +72,35 @@ def kmer_engine(FQ, des_ref, cont_refs, k = 10, tolerance = 5): #requires the de
 
         idx_count = 0
         for des_idx in des_idxs:
+
             #check if the k_mers are in the desired reference
             kmer_locs = []
             for mer in read_kmers:
-                if mer in des_idx:
-                    kmer_locs.append(des_idx[mer])
 
+                if mer in des_idx.keys(): ###here is the issue
+                    kmer_locs.append(des_idx[mer])
+            #print(kmer_locs)
             #Check for alignments to the desired reference
             if len(kmer_locs) >= len(read_kmers)-((k)*tolerance) and len(kmer_locs) > 0:
+#working here
                 #need to check the reference and calculate the hamming distance each start, as well as k, 2k, ... and (tolerance)k before it
-                for start in kmer_locs[0]:
-                    starts = [start]
-                    for i in range(tolerance):
-                        if starts[len(starts)-1] - k >= 0:
-                            starts.append(starts[len(starts)-1] - k)
-
+                new_starts = kmer_locs[0]
+                    #need to check the reference and calculate the hamming distance each start, as well as k, 2k, ... and (tolerance)k before it
                     #need to compare the read to each read length section of the reference begining at each start point
-                    for strt in starts:
-                        num_mismatch = 0
-                        compare = des_FA[idx_count][strt:strt+len(read)]
-                        for base_num in range(len(read)):
-                            base = read[base_num]
-                            if base_num >= len(compare):
-                                #num_mismatch = tolerance +1
+                for strt in new_starts:
+                    num_mismatch = 0
+                    compare = des_FAs[idx_count][strt:strt+len(read)]
+                    for base_num in range(len(read)):
+                        base = read[base_num]
+                    #to prevent checking  past edge of reference
+                        if base_num >= len(compare):
+                            #num_mismatch = tolerance +1
+                            break
+                        else:
+                            if base != compare[base_num]:
+                                num_mismatch += 1
+                            if num_mismatch > tolerance:
                                 break
-                            else:
-                                if base != compare[base_num]:
-                                    num_mismatch += 1
-                                if num_mismatch > tolerance:
-                                    break
                      #add aligned reads to the dictionary to output
                     if num_mismatch <= tolerance and which_read-1 not in count_good:
                         des_count = des_count + 1
