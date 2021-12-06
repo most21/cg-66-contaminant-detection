0a781ed3531dde036e24bd8cb69e98aa9e68b6c2
asolgan1 <solganik.alexey@gmail.com>
2021-12-06 17:14:35 -0500

    Complete FM

--- FM_index.py
+++ FM_index.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Mon Dec  6 17:13:06 2021
+
+@author: Alex
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Nov 10 17:05:03 2021
 
 @author: Alex
 """
 
 import copy as cp
+import numpy as np
+import math
+from data_utils1 import read_fasta_files, read_fastq_files
 
 def rotations(t):
     ''' Return list of rotations of input string t '''
@@ -47,15 +58,6 @@ def firstCol(tots):
     return first
 
 
-t = 'abaabab$'
-b = bwtViaBwm(t)
-
-ranks, tots = rankBwt(b)
-print(ranks)
-
-print(b)
-print(firstCol(tots))
-
 
 def reverseBwt(bw):
     ''' Make T from BWT(T) '''
@@ -73,11 +75,107 @@ def reverseBwt(bw):
         rowi = first[c][0] + ranks[rowi]
     return t
 
+#########################################
+# Suffix array functions
+# https://louisabraham.github.io/notebooks/suffix_arrays.html
+
+from itertools import zip_longest, islice
+
+def to_int_keys_best(l):
+    """
+    l: iterable of keys
+    returns: a list with integer keys
+    """
+    seen = set()
+    ls = []
+    #print(len(l))
+    for k in range(0,len(l)):
+        
+        e = l[k]
+        if not e in seen:
+            ls.append(e)
+            seen.add(e)
+    ls.sort()
+    index = {v: i for i, v in enumerate(ls)}
+    return [index[l[j]] for j in range(0,len(l))]
+
+
+def suffix_array_inverse(s):
+    """
+    suffix array of s
+    O(n * log(n)^2)
+    """
+    n = len(s)
+    k = 1
+    line = to_int_keys_best(s)
+    while max(line) < n - 1:
+        line = to_int_keys_best(
+            [a * (n + 1) + b + 1
+             for (a, b) in
+             zip_longest(line, islice(line, k, None),
+                         fillvalue=-1)])
+        k <<= 1
+    return line    
+
+def final_sa(l):
+    """
+    Inverses sa from above
+    """
+    n = len(l)
+    ans = [0] * n
+    for i in range(n):
+        ans[l[i]] = i
+    return ans  
+
+
+def final_sa1(l,factor):
+    """
+    Inverses sa from above
+    """
+    n = len(l)
+    ans = [0] * n
+    for i in range(n):
+        ans[l[i]] = i
+    dic = {}
+    for i in range(len(ans)):
+        if i % factor == 0:
+            dic[i] = ans[i]
+    return dic
+
+
+
+######################################
+#Functions I wrote
+
+def totsBWT(bw):
+    '''Map of characters to number of times it appears '''
+    tots = dict()
+    for c in bw:
+        if c not in tots:
+            tots[c] = 0
+        tots[c] += 1
+    return tots
+
+
+
+def firstColMod(tots):
+    ''' Return map from character to the range of rows prefixed by
+        the character. I changed the function to 1-indexed vs 
+        0-indexed before'''
+    first = {}
+    totc = 1
+    for c, count in sorted(tots.items()):
+        first[c] = (totc, totc + count)
+        totc += count
+    return first
+
 
 def modifiedRankBWT(bw,step):
-    _, tots = rankBwt(b)
-    f = firstCol(tots)
+    '''Function that applies the ranking heuristics number 1 and 2 from class'''
+    tots = totsBWT(bw)
+    f = firstColMod(tots)
     tots = dict()
+    total = 0
 
     for i in f:
         f[i] = []
@@ -89,24 +187,34 @@ def modifiedRankBWT(bw,step):
         c = bw[i]
         if bw[i] not in tots:
             tots[c] = 0
-        f1[c].append(tots[c]+1)
+        
+        total += 1
         tots[c] += 1
-
+        f1[c].append([total, tots[c]])
+        
+        
         for j in f1:
             if j != c:
-                #print(first)
+
                 if len(f1[j]) == 0:
                     if first == False:
-                        f1[j].append(f[j][-1])               
+                        last_entry_from_main_table = cp.deepcopy(f[j][-1])
+                        last_entry_from_main_table[0] +=1
+                        f1[j].append(last_entry_from_main_table)               
                     else:
-                       f1[j].append(0)
+                       
+                       f1[j].append([1,0])
                 else:
-                    f1[j].append(f[j][-1])  
+                    previous_entry =  cp.deepcopy(f1[j][-1])
+                    previous_entry[0] +=1
+                    f1[j].append(previous_entry)  
         first = False            
         if i % step == 0:
             for k in f:
                 f[k].append(f1[k][-1])
             f1 = cp.deepcopy(f2)  
+    for i in f:
+        f[i] = np.asarray(f[i])
        
     return f
 
@@ -116,10 +224,209 @@ def modifiedRankBWT(bw,step):
 
 
 
-#def queryFM(bw):
-    #''' Given a bw and another string check if 
+def find_nearest(array,value):
+    '''Helper function ot find nearest value in the ranking data structure'''
+    idx = np.searchsorted(array[:,0], value, side="left")
+    
+    if idx > 0 and (idx == len(array[:,0]) or math.fabs(value - array[:,0][idx-1]) < math.fabs(value - array[:,0][idx])):
+        return array[idx-1]
+    else:
+        return array[idx]
+                       
+    
 
 
-#print(b)
-#print(reverseBwt(bwtViaBwm("Tomorrow_and_tomorrow_and_tomorrow$")))
+def queryBWT(pattern, index_table, bw):
+    '''Function to return the FM left column indices of where the pattern occurs '''
+    # Get the last char of the pattern
+    """ Reverse the order of the string for ease"""
+    reverse_pattern = pattern[::-1]
+    length = len(reverse_pattern)
+    
+    start = 0
+    """ Start with the 1st char of theis string"""
+    starting_char = reverse_pattern[start]
+    
+    tots = totsBWT(bw)
+    f = firstColMod(tots)
+    """ Get the first range of characters from the first column"""
+    first_range = f[starting_char]
 
+    final_rank = 0
+   
+    next_range = range(first_range[0], first_range[1])       
+    next_range1 = []
+
+    for k in range(1, len(pattern)):
+        start += 1
+        """ If there is a next character in the reverse_pattern"""
+        if start < length:
+            """ Go through the range of characters in the first column"""
+            for j in range(len(next_range)): 
+                # bw is 0-indexed so have to subtract 1
+                """ Goet the next character"""
+                c = reverse_pattern[start]
+
+                i = next_range[j]
+
+                
+                """ Compare the next character with a character at the same index 
+                from BWT. Have to subtract 1 because BWT is 0-indexed."""
+                if bw[i-1] == c:
+
+                
+                    """In the dictonary go to the 'column' of the desired 
+                    character and find the nearest available total index"""
+                    closest_index = find_nearest(index_table[c], i)
+                    """If the nearest available total index is the actual total
+                    index then we can return the corresponding char-specific index.
+                    That will be the actual index of the character."""
+                    if closest_index[0] == i:
+                        final_rank = closest_index[1]
+                        #print(final_rank)
+                    """If the nearest available total index is smaller than 
+                    the actual total index, we have to walk down bwt"""
+                    
+                    if closest_index[0] < i:
+                        # Subtract 1 because bw is 0-indexed
+                        closest_in_bw = closest_index[0] 
+                        final_rank = closest_index[1]
+                        for j in range(closest_in_bw, i):
+                            if bw[j] == c:
+                                final_rank += 1       
+                    if closest_index[0] > i:
+                        # Subtract 1 because bw is 0-indexed
+                        closest_in_bw = closest_index[0] 
+                        final_rank = closest_index[1]
+                        for j in range(i, closest_in_bw):
+                            if bw[j] == c:
+                                final_rank -= 1    
+                
+                    """After we have found the index of the character, need to 
+                    find where it is in the left column"""
+                
+                    transition_index = f[c][0] + final_rank -1
+                    next_range1.append(transition_index) 
+                
+            next_range = cp.deepcopy(next_range1)        
+            next_range1 = []    
+
+            
+            
+    return next_range
+
+
+def locate_in_template(template, pattern):
+    '''Function to query the pattern in the template with SA with no gaps'''
+    b = bwtViaBwm(template)
+    m = modifiedRankBWT(b,3)
+    ranks = queryBWT(pattern, m, b)
+    actual_indexes = []
+    sa = final_sa(suffix_array_inverse(template))
+    for i in range(len(ranks)):
+        actual_indexes.append(sa[ranks[i]-1])
+    return actual_indexes    
+        
+    
+    
+
+def construct_fm(template, factor):
+    b = bwtViaBwm(template)
+    m = modifiedRankBWT(b,factor)
+    
+    
+    sa = final_sa1(suffix_array_inverse(template),factor) 
+    tots = totsBWT(b)
+    f = firstColMod(tots)
+    return [b, m,f, sa]
+
+
+
+def locate_in_template1(fm,pattern):
+    '''Function to query the pattern in the template with SA with gaps'''
+    """b = bwtViaBwm(template)
+    m = modifiedRankBWT(b,factor)
+    ranks = queryBWT(pattern, m, b)
+    
+    sa = final_sa1(suffix_array_inverse(template),factor) 
+    tots = totsBWT(b)
+    """
+    b = fm[0]
+    m = fm[1]
+
+    f = fm[2]
+    sa = fm[3]
+    
+    
+    ranks = queryBWT(pattern, m, b)
+    actual_indexes = []
+
+    
+
+    for i in range(len(ranks)):
+        
+        if ranks[i]-1 in sa:
+            actual_indexes.append(sa[ranks[i]-1])
+        else:
+            count  = 0
+            transition_index = ranks[i]
+            while transition_index-1 not in sa:
+             
+                ch = b[transition_index-1]
+                closest_index = find_nearest(m[ch], transition_index)
+                final_rank = 0
+                if closest_index[0] == transition_index:
+                    final_rank = closest_index[1]
+                    
+                if closest_index[0] < transition_index:
+                           # Subtract 1 because bw is 0-indexed
+                    closest_in_bw = closest_index[0] 
+                    final_rank = closest_index[1]
+                    for j in range(closest_in_bw, transition_index):
+                        if b[j] == ch:
+                            final_rank += 1       
+                if closest_index[0] > transition_index:
+                           # Subtract 1 because bw is 0-indexed
+                   closest_in_bw = closest_index[0] 
+                   final_rank = closest_index[1]
+                   for j in range(transition_index, closest_in_bw):
+                       if b[j] == ch:
+                           final_rank -= 1    
+ 
+                transition_index = f[ch][0] + final_rank -1
+                count +=1
+            actual_indexes.append((sa[transition_index-1]+count)%len(b))
+            
+    return [actual_indexes, len(ranks)]
+            
+    
+    
+    
+    
+def everything_combined(fastas, fastqs, factor):
+    
+    fasta_objects = read_fasta_files(fastas)
+    fastq_objects = read_fastq_files(fastqs)
+    
+    fms = []
+    for fasta in fasta_objects:
+        for i,template in enumerate(fasta):
+
+            fms.append([fasta.ids[i], construct_fm(template, factor)])
+        
+    for fm in fms:
+        for fastq in fastq_objects:
+            for fastq_dict in fastq:
+                pattern = fastq_dict['seq']
+                fastq_id = fastq_dict['id']
+                info = locate_in_template1(fm[1],pattern)
+                
+                print("The fasta id: ", fm[0])
+                print("The pattern is: %s" % (fastq_id))
+                print("The positions in the template where pattern occurs: ", info[0])
+                print("The number of pattern occurences: ", info[1])
+
+
+
+
+everything_combined(['tinydataexample/example1.fasta'],['tinydataexample/chr1_Mfermentans_8020_hiseq_reads_tinycut_R1.fastq'],10)
